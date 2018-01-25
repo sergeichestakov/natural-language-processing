b24bb745ab6f7a0df8d7d8680dcbfe44533b9d54
Sergei Chestakov <sergei332@gmail.com>
2018-01-24 18:14:02 -0800

    implement smooth unigram model

--- hw1/UnigramModel.py
+++ hw1/UnigramModel.py
@@ -2,7 +2,7 @@ import math, collections
 
 class UnigramModel:
   """Language model that uses unigram probabilities, ignoring unseen words."""
-
+  
   def __init__(self, corpus):
     self.unigramCounts = collections.defaultdict(lambda: 0)
     self.total = 0
@@ -11,14 +11,14 @@ class UnigramModel:
   def train(self, corpus):
     """Takes a HolbrookCorpus corpus, does whatever training is needed."""
     for sentence in corpus.corpus:
-      for datum in sentence.data:  
+      for datum in sentence.data:
         token = datum.word
         self.unigramCounts[token] = self.unigramCounts[token] + 1
         self.total += 1
-  
+
   def score(self, sentence):
     """Takes a list of strings, returns a score of that sentence."""
-    score = 0.0 
+    score = 0.0
     for token in sentence:
       count = self.unigramCounts[token]
       if count > 0:
